3a997510ffa803d6c323df09017c9a3beaaeec33
Leonardo Lana <leoclana@gmail.com>
2018-06-14 09:03:06 -0300

    3.6. Cadastrando nova categoria com POST
    POST - localhost:8080/categorias (HEADERS : Content-Type = application/json ,  Body-raw : { "nome" : "Financiamento" } ).
    GET - http://localhost:8080/categorias/6

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,12 +1,19 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
@@ -44,6 +51,36 @@ public class CategoriaResource {
 		retorno = ( !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build()); 
 		
 		return retorno;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		
+		//Categoria categoriaSalva = 
+		categoriaRepository.save(categoria);
+		
+		/**
+		 * ServletUriComponentsBuilder.fromCurrentRequestUri().path : pega a URL origem da requisicao 
+		 * ("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()) : adiciona o codigo da categoria no path
+		 * .toUri();                                                : converte em um URT
+		 */
+		URI uri = 
+			ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoria.getCodigo())
+			.toUri();
+		
+		/**
+		 * Seta um novo atributo "Location" no "Headers" do "HTTP response" a URL para consultar, via requisiao, a categoria Criada
+		 * Ex.: http://localhost:8080/categorias/6
+		 */
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoria);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findOne(codigo);
 	}
 	
 }
