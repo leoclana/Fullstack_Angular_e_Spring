9421789dd9c5df850d925de52f950f2817e86dd7
Leonardo Lana <leoclana@gmail.com>
2018-06-26 07:49:10 -0300

    5.3. Desafio: Cadastrando o primeiro lançamento

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -2,10 +2,15 @@ package com.example.algamoney.api.resource;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +34,12 @@ public class LancamentoResource {
 	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo){
 		Lancamento lancamento = lancamentoRepository.findOne(codigo);
 		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento){
+		lancamentoRepository.save(lancamento);
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamento);
 	}
 
 }
