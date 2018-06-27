829563c12f2196bc78779595200d32d4d8e55187
Leonardo Lana <leoclana@gmail.com>
2018-06-27 10:23:42 -0300

    5.6. Regra para não salvar pessoa inativa

--- algamoney-api/src/main/java/com/example/algamoney/api/service/PessoaService.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -22,14 +22,13 @@ public class PessoaService {
 		return pessoaRepository.save(pessoaSalva);
 	}
 
-	
 	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
 		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
 		pessoaSalva.setAtivo(ativo);
 		pessoaRepository.save(pessoaSalva);
 	}
 
-	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+	public Pessoa buscarPessoaPeloCodigo(Long codigo) {
 		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
 		if(pessoaSalva == null) {
 			throw new EmptyResultDataAccessException(1);
